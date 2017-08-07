a2bf75472b8084f18904d4b2bf31f475d9fcd56f
kishoremallick <kishore.mallick@outlook.com>
2017-08-07 11:31:29 +0530

    test login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+ model: any = {};
+  loginError = "";
+  loginErr=false;
+  statusMsg;
+  constructor(private router: Router,) { }
 
   ngOnInit() {
   }
+   login() {
+        
+         if(this.model.username =="test" &&this.model.password =="test"){
+            this.router.navigate(['dashboard/home']);
+         }else{
+                this.loginErr=true;
+                this.loginError = "Login failed.";
+               
+        }
+
+    }
 
 }
